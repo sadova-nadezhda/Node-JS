ba959e3fd79960526cd4838d3289174ac7654bfd
sadova-nadezhda <109034129+sadova-nadezhda@users.noreply.github.com>
2023-12-22 21:34:14 +0600

    update lesson-4

--- lesson-4/project1/index.js
+++ lesson-4/project1/index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const path = require('path');
 const { error } = require('console');
 
 const PORT = 3000;
@@ -7,17 +8,41 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   console.log('Server request')
   res.setHeader('Content-Type', 'text/html')
-  if (req.url == '/') {
-    fs.readFile('./views/index.html', (error, data) => {
-      if (error) {
-        console.log(error);
-        res.end();
-      } else {
-        res.write(data);
-        res.end();
-      }
-    });
+  let basePath = '';
+  const createParh = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+  switch (req.url) {
+    case '/':
+      basePath = createParh('index');
+      res.statusCode = 200;
+      break;
+    case '/about-us':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
+      break;
+    case '/about':
+      basePath = createParh('about');
+      res.statusCode = 200;
+      break;
+    case '/contact':
+      basePath = createParh('contact');
+      res.statusCode = 200;
+      break;
+    default:
+      basePath = createParh('404');
+      res.statusCode = 404;
+      break;
   }
+  fs.readFile(basePath, (error, data) => {
+    if (error) {
+      console.log(error);
+      res.statusCode = 500;
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(PORT, 'localhost', (error) => {
