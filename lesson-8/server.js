65eebdd356e5705a5fa55a82b5b70a7bb4061033
sadova-nadezhda <109034129+sadova-nadezhda@users.noreply.github.com>
2024-01-11 19:58:48 +0600

    Removed node_modules folder and added to .gitignore

---
+++ lesson-8/server.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const News = require('./models/post');
+const Socials = require('./models/socials');
+const { error } = require('console');
+
+const port = 3000;
+const app = express();
+const db = 'mongodb+srv://admin:Pass123@cluster0.lwcbfjr.mongodb.net/node_news?retryWrites=true&w=majority';
+
+app.set('view engine', 'ejs');
+
+const createParh = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
+
+mongoose.connect(db).then(() => {
+  console.log('Connect to DB');
+}).catch((error) => {
+  console.log(error);
+})
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({
+  extended: false
+}));
+
+app.get('/', (req, res) => {
+  const title = 'Home'
+  res.render(createParh('index'), { title });
+});
+app.get('/news', (req, res) => {
+  const title = 'News';
+  const posts = [
+    {
+      id: 1,
+      text: 'test',
+      title: 'title 1',
+      date: '12.12.2023',
+      author: 'Author'
+    }
+  ]
+  res.render(createParh('news'), { title, posts });
+});
+app.get('/news/:id', (req, res) => {
+  const title = 'News Inner';
+  const post = {
+    id: 1,
+    text: 'test',
+    title: 'title',
+    date: '12.12.2023',
+    author: 'Author'
+  }
+  res.render(createParh('news-inner'), { title, post });
+});
+app.get('/news-app', (req, res) => {
+  const title = 'News App'
+  res.render(createParh('news-app'), { title });
+});
+app.post('/news-app', (req, res) => {
+  const { title, author, text } = req.body;
+  const post = new News({ title, author, text })
+  post
+    .save()
+    .then((result) => res.send(result))
+    .catch((error) => {
+      console.log(error)
+      res.render(createParh('404'), { title: 'Error' });
+    })
+});
+app.get('/contact', (req, res) => {
+  const title = 'Contact'
+  Socials
+    .find()
+    .then((socials) => res.render(createParh('contact'), { title, socials }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createParh('404'), { title: 'Error' });
+    })
+
+});
+app.use((req, res) => {
+  const title = 'PageNotFound'
+  res
+    .status(404)
+    .render(createParh('404'), { title })
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port http://localhost:${port}`)
+});
