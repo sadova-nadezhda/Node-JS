7e9ad777b6157eab263e2e8e117aaa6017ac3e3e
sadova-nadezhda <109034129+sadova-nadezhda@users.noreply.github.com>
2024-01-11 21:43:06 +0600

    update lesson-8

--- lesson-8/server.js
+++ lesson-8/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const News = require('./models/post');
 const Socials = require('./models/socials');
 const { error } = require('console');
@@ -24,6 +25,7 @@ app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({
   extended: false
 }));
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   const title = 'Home'
@@ -31,27 +33,55 @@ app.get('/', (req, res) => {
 });
 app.get('/news', (req, res) => {
   const title = 'News';
-  const posts = [
-    {
-      id: 1,
-      text: 'test',
-      title: 'title 1',
-      date: '12.12.2023',
-      author: 'Author'
-    }
-  ]
-  res.render(createParh('news'), { title, posts });
+  News
+    .find()
+    .then((posts) => res.render(createParh('news'), { title, posts }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createParh('404'), { title: 'Error' });
+    })
+
 });
 app.get('/news/:id', (req, res) => {
   const title = 'News Inner';
-  const post = {
-    id: 1,
-    text: 'test',
-    title: 'title',
-    date: '12.12.2023',
-    author: 'Author'
-  }
-  res.render(createParh('news-inner'), { title, post });
+  News
+    .findById(req.params.id)
+    .then((post) => res.render(createParh('news-inner'), { title, post }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createParh('404'), { title: 'Error' });
+    })
+});
+app.delete('/news/:id', (req, res) => {
+  News
+    .findByIdAndDelete(req.params.id)
+    .then((post) => res.sendStatus(200))
+    .catch((error) => {
+      console.log(error);
+      res.render(createParh('404'), { title: 'Error' });
+    })
+});
+
+app.get('/edit/:id', (req, res) => {
+  const title = 'Edit News';
+  News
+    .findById(req.params.id)
+    .then((post) => res.render(createParh('news-edit'), { title, post }))
+    .catch((error) => {
+      console.log(error);
+      res.render(createParh('404'), { title: 'Error' });
+    })
+});
+app.put('/edit/:id', (req, res) => {
+  const { title, author, text } = req.body;
+  const { id } = req.params;
+  News
+    .findByIdAndUpdate(id, { title, author, text })
+    .then((result) => res.redirect(`/news/${id}`))
+    .catch((error) => {
+      console.log(error);
+      res.render(createParh('404'), { title: 'Error' });
+    })
 });
 app.get('/news-app', (req, res) => {
   const title = 'News App'
@@ -62,7 +92,7 @@ app.post('/news-app', (req, res) => {
   const post = new News({ title, author, text })
   post
     .save()
-    .then((result) => res.send(result))
+    .then((result) => res.redirect('/news'))
     .catch((error) => {
       console.log(error)
       res.render(createParh('404'), { title: 'Error' });
@@ -77,7 +107,6 @@ app.get('/contact', (req, res) => {
       console.log(error);
       res.render(createParh('404'), { title: 'Error' });
     })
-
 });
 app.use((req, res) => {
   const title = 'PageNotFound'
